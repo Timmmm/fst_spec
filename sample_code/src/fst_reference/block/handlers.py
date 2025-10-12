6b2afaeedf3d30bc61efd3ad1695d6881ab6c3ff
Tim Hutt <tdhutt@gmail.com>
2025-10-12 09:11:13 +0100

    Use .value for int enums instead of int() cast

--- sample_code/src/fst_reference/block/handlers.py
+++ sample_code/src/fst_reference/block/handlers.py
@@ -37,18 +37,18 @@ def _unsupported_block_handler(
 
 
 BLOCKS = {
-    int(BlockType.HDR): {"name": "HDR", "handler": CallHDR},
-    int(BlockType.BLACKOUT): {"name": "BLACKOUT", "handler": CallBLACKOUT},
-    int(BlockType.GEOM): {"name": "GEOM", "handler": CallGEOM},
-    int(BlockType.HIER_GZ): {"name": "HIER_GZ", "handler": CallHIER_GZ},
-    int(BlockType.HIER_LZ4): {"name": "HIER_LZ4", "handler": CallHIER_LZ4},
-    int(BlockType.HIER_LZ4DUO): {"name": "HIER_LZ4DUO", "handler": CallHIER_LZ4DUO},
-    int(BlockType.VCDATA): {"name": "VCDATA", "handler": _unsupported_block_handler},
-    int(BlockType.VCDATA_DYN_ALIAS): {
+    BlockType.HDR.value: {"name": "HDR", "handler": CallHDR},
+    BlockType.BLACKOUT.value: {"name": "BLACKOUT", "handler": CallBLACKOUT},
+    BlockType.GEOM.value: {"name": "GEOM", "handler": CallGEOM},
+    BlockType.HIER_GZ.value: {"name": "HIER_GZ", "handler": CallHIER_GZ},
+    BlockType.HIER_LZ4.value: {"name": "HIER_LZ4", "handler": CallHIER_LZ4},
+    BlockType.HIER_LZ4DUO.value: {"name": "HIER_LZ4DUO", "handler": CallHIER_LZ4DUO},
+    BlockType.VCDATA.value: {"name": "VCDATA", "handler": _unsupported_block_handler},
+    BlockType.VCDATA_DYN_ALIAS.value: {
         "name": "VCDATA_DYN_ALIAS",
         "handler": _unsupported_block_handler,
     },
-    int(BlockType.VCDATA_DYN_ALIAS2): {
+    BlockType.VCDATA_DYN_ALIAS2.value: {
         "name": "VCDATA_DYN_ALIAS2",
         "handler": CallVCDATA,
     },
